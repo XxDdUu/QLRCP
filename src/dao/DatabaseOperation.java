aa0c90fb51d989c96c47db3fc5444e6cc043cb27
XxDdUu <tranquangduy15122006@gmail.com>
2024-12-25 22:33:56 +0700

    Create DatabaseOperation.java

---
+++ src/dao/DatabaseOperation.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseOperation {
+	public Connection connectToDataBase() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(Constant.String_DB_Url, Constant.username, Constant.password);
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		return connection;
+	}
+	public int ExcuteUpdate(String sql, Object[] values) {
+		int rowAffected = 0;
+		try (Connection connection = connectToDataBase()){
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			for (int i = 0; i < values.length; i++) {
+				preparedStatement.setObject(i + 1, values[i]);
+			}
+			rowAffected = preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rowAffected;
+	}
+}
