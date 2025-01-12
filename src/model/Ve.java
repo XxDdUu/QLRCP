f4796d5fcaddb3318af67cdd633e2cb2b9486e4f
Yuuta0504 <Bachnd2006@gmail.com>
2025-01-13 01:21:25 +0700

    Chinh sua thuoc tinh

--- src/model/Ve.java
+++ src/model/Ve.java
@@ -1,6 +1,6 @@
 package model;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Ve
 {
@@ -10,43 +10,56 @@ public class Ve
 	private java.util.Date NgayMua;
 	private String LoaiVe;
 	private float GiaTien;
+
 	public String getMaVE() {
 		return MaVE;
 	}
+
 	public void setMaVE(String maVE) {
 		MaVE = maVE;
 	}
+
 	public String getMaTV() {
 		return MaTV;
 	}
+
 	public void setMaTV(String maTV) {
 		MaTV = maTV;
 	}
+
 	public String getMaLC() {
 		return MaLC;
 	}
+
 	public void setMaLC(String maLC) {
 		MaLC = maLC;
 	}
-	public java.util.Date getNgayMua() {
+
+	public Date getNgayMua() {
 		return NgayMua;
 	}
-	public void setNgayMua(java.util.Date ngayMua) {
+
+	public void setNgayMua(Date ngayMua) {
 		NgayMua = ngayMua;
 	}
+
 	public String getLoaiVe() {
 		return LoaiVe;
 	}
+
 	public void setLoaiVe(String loaiVe) {
 		LoaiVe = loaiVe;
 	}
+
 	public float getGiaTien() {
 		return GiaTien;
 	}
+
 	public void setGiaTien(float giaTien) {
 		GiaTien = giaTien;
 	}
-	public Ve(String maVE, String maTV, String maLC, java.util.Date ngayMua, String loaiVe, float giaTien) {
+
+	public Ve(String maVE, String maTV, String maLC, Date ngayMua, String loaiVe, float giaTien) {
 		MaVE = maVE;
 		MaTV = maTV;
 		MaLC = maLC;
@@ -54,5 +67,4 @@ public class Ve
 		LoaiVe = loaiVe;
 		GiaTien = giaTien;
 	}
-	
 }
